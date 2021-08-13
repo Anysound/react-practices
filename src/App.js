ff92f6b01e848a611b72a96c7f0f7b0583e44d6a
i.heria <i.heria@andersenlab.com>
2021-08-13 16:19:31 +0300

    redux, action-creators

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./App.module.css";
+import { addCustomerAction, removeCustomerAction } from "./store/customerReducer";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash.cash);
+  const customers = useSelector((state) => state.customers.customers);
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    };
+    dispatch(addCustomerAction(customer));
+  };
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>Баланс: {cash}</p>
+        <button
+          className={styles.button}
+          onClick={() => addCash(+prompt("сумма пополнения: "))}
         >
-          Learn React
-        </a>
+          Пополнить счет
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => getCash(+prompt("сумма снятия: "))}
+        >
+          Снять со счета
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => addCustomer(prompt("Добавить клиента: "))}
+        >
+          Добавить клиента
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => getCash(+prompt("сумма снятия: "))}
+        >
+          Удалить клиента
+        </button>
+        {customers.length > 0 ? (
+          <div>
+            {customers.map((it) => (
+              <div key={Date.now()} onClick={() => removeCustomer(it)}>{it.name}</div>
+            ))}
+          </div>
+        ) : (
+          <div>Клиентов нет</div>
+        )}
       </header>
     </div>
   );
