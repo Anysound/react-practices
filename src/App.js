5fdae70b0d07b8e0905675f7c79296910dcbf4eb
n.bergaliev <anysoundspb@gmail.com>
2021-10-23 14:01:26 +0300

    added request API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useMemo, useRef, useState } from "react";
+import axios from "axios";
+import { useEffect, useMemo, useRef, useState } from "react";
+import PostService from "./API/PostService";
 import "./App.css";
 import { usePosts } from "./hooks/usePosts";
 import { PostFilter } from "./PostFilter";
@@ -19,6 +21,7 @@ function App() {
   ]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [isPostsLoading, setIsPostsLoading] = useState(false)
 
   const bodyInputRef = useRef(); // получаем ссылку для элемента
   const createPost = (newPost) => {
@@ -26,25 +29,37 @@ function App() {
     setModal(false);
   };
 
-  function getSortedPosts() {
-
-  }
+  async function fetchPosts() {
+    setIsPostsLoading(true);
+    const data = await PostService.getAll();
+    // const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    setPosts(data);
+    setIsPostsLoading(false)
+  } 
 
   const deletePost = (post) => {
     setPosts(posts.filter((it) => it.id !== post.id));
   };
 
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  useEffect(() => {
+    fetchPosts();
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={(evt) => {
+          console.log('evt: ', evt);
+          fetchPosts();
+        }}>get posts</button>
+        {isPostsLoading ? <h1>posts are loading...</h1> : <PostList posts={sortedAndSearchedPosts} remove={deletePost} /> }
         <MyBtn onClick={() => setModal(true)}>create User</MyBtn>
         <Modal visible={modal} setVisible={setModal}>
           <PostForm create={createPost} />
         </Modal>
-        <PostList posts={sortedAndSearchedPosts} remove={deletePost} />
+        
         <PostFilter filter={filter} setFilter={setFilter} />
       </header>
     </div>
